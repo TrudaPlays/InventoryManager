9178d89add5acafc27d41051df2ca6af8b6b3e9f
TrudaPlays <gertrude.m.fleischmann@gmail.com>
2026-03-24 17:15:50 -0500

    added database Inventory and added a table Products with the required columns

--- InventoryManager/Form1.cs
+++ InventoryManager/Form1.cs
@@ -19,26 +19,6 @@ namespace InventoryManager
 
         private void InventoryManagerForm_Load(object sender, EventArgs e)
         {
-            // Skip loading at design time
-            if (this.DesignMode) return;
-
-            try
-            {
-                // Ensure required designer components are available
-                if (productsTableAdapter1 == null || inventoryDatabaseDataSet1 == null)
-                    return;
-
-                // Clear existing rows to ensure a fresh load
-                inventoryDatabaseDataSet1.Products.Clear();
-
-                // Load products into dataset
-                this.productsTableAdapter1.Fill(this.inventoryDatabaseDataSet1.Products);
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("Unable to load products. Please contact support.", "Load error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                // Consider logging the exception details to a file or telemetry
-            }
 
         }
     }
--- InventoryManager/Properties/Settings.Designer.cs
+++ InventoryManager/Properties/Settings.Designer.cs
@@ -44,5 +44,16 @@ namespace InventoryManager.Properties {
                 return ((string)(this["InventoryDatabaseConnString"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\gertr\\Documents\\Inve" +
+            "ntory.mdf;Integrated Security=True;Connect Timeout=30")]
+        public string InventoryConnectionString {
+            get {
+                return ((string)(this["InventoryConnectionString"]));
+            }
+        }
     }
 }
